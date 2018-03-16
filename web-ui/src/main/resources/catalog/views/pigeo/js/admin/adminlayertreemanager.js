eae2b065f9f03ea8876e6f22d61db2747e41c63e
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2018-03-16 17:44:40 +0100

    Layertree supports new node type 'wmsdem'
    Missing : it doesn't read them, thus won't display them after you create them

--- web-ui/src/main/resources/catalog/views/pigeo/js/admin/adminlayertreemanager.js
+++ web-ui/src/main/resources/catalog/views/pigeo/js/admin/adminlayertreemanager.js
@@ -59,7 +59,7 @@
 
       $scope.getLayer = function(node) {
         var type = node.type;
-        if(type == 'wms' || type == 'wmts' || type == 'chart') {
+        if(type == 'wms' || type == 'wmts' || type == 'wmsdem' || type == 'chart') {
           return true;
         }
       };
